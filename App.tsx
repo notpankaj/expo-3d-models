83e752e81965de118018c08400814780e682fff5
notpankaj <iampankaj0409@gmail.com>
2022-09-18 10:52:08 +0530

    test

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from "react-native";
+import React from "react";
+import Knife from "./Knife";
+import Dog from "./Dog";
+
+const App = () => {
+  return <Knife />;
+  return <Dog />;
+  return (
+    <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
+      <Text style={{ color: "red" }}>App</Text>
+    </View>
+  );
+};
+
+export default App;
